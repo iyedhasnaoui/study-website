c0d2d4f91f333fe04165b4847894ccd509b51908
Karim Keraani <karimkeraani24@gmail.com>
2026-09-02 17:31:24 +0200

    fix: fix conflict between data.sql data initialization and tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO users (id, username, email, password_hash) -- add dummy user to debug ForumPostController, else is it a foreign key constraint violation error --
 VALUES (1, 'testuser', 'test@example.com', 'password_hash')
 ON CONFLICT (id) DO NOTHING;
+
+SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id), 0) + 1, false) FROM users; -- to avoid conflicts with the tests
